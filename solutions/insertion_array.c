7aac3ee681e057c6b87ccda708fb0799592a7412
SahilArvind <sahilzade2310@gmail.com>
2024-12-15 02:47:57 +0530

    fixed the logical problem

--- solutions/insertion_array.c
+++ solutions/insertion_array.c
@@ -1,8 +1,6 @@
-// Q) Write a C program to insert an element into an array.
-
 #include<stdio.h>
 
-void displayArray(int arr[], int n){
+void displayArray(int arr[], int n) {
     printf("[ ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -10,25 +8,25 @@ void displayArray(int arr[], int n){
     printf("]\n");
 }
 
-int indInsertion(int arr[], int size, int index, int element) {
-    for (int i = size; i >= index; i--) {
-        arr[i+1] = arr[i];
+void indInsertion(int arr[], int size, int index, int element) {
+    for (int i = size - 1; i >= index; i--) {
+        arr[i + 1] = arr[i];
     }
     arr[index] = element;
-
-    return 0;
 }
 
-int main () {
+int main() {
     int size;
     printf("Enter the size of the array: ");
     scanf("%d", &size);
 
-    int arr[size+1];
+    int arr[size + 1];
+
     printf("Enter %d elements into the array:\n", size);
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
+
     printf("The array you created is: ");
     displayArray(arr, size);
 
@@ -36,11 +34,19 @@ int main () {
     printf("Enter the index at which you want to insert an element: ");
     scanf("%d", &index);
 
+    if (index < 0 || index > size) {
+        printf("Error: Invalid index. Please enter an index between 0 and %d.\n", size);
+        return 1;
+    }
+
     int element;
     printf("Enter the element you want to insert: ");
     scanf("%d", &element);
 
     indInsertion(arr, size, index, element);
+
     printf("The array after inserting the new element: ");
-    displayArray(arr, size+1);
+    displayArray(arr, size + 1);
+
+    return 0;
 }
